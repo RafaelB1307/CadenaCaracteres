38c8e26c6aca4b10ac0eef29264f010688d351f5
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 01:06:28 -0700

    Agregar archivos de proyecto.

---
+++ Readline/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+
+            //Preguntamos el nombre del usuario
+            Console.WriteLine("¿Como te llamas?: ");
+            nombre = Console.ReadLine(); //Aqui se guarda la respuesta en la variable
+
+            Console.WriteLine("Hola " + nombre + " ¿Quieres ver el menu? ");
+            Console.ReadKey();
+
+        }
+    }
+}
